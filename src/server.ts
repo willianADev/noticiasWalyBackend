8ff66caa58636a20d9351a3c7f27a4913a1ed1a3
Willian Andres Almendras Calizaya <willian.a.almendras@gmail.com>
2026-03-03 02:34:48 -0400

    Fix: arreglo de server

--- src/server.ts
+++ src/server.ts
@@ -5,12 +5,14 @@ import cors from 'cors';
 import { entorno } from './config/env';
 import rutasAutenticacion from './routes/auth.routes';
 import rutasNoticias from './routes/noticia.routes';
+import path from 'node:path';
 
 const app: Application = express();
 
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+app.use('/public', express.static(path.join(process.cwd(), 'public')));
 app.set('json spaces', 2);
 
 // Rutas
